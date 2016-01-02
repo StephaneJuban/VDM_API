f7347af9dfbb4d3f65a72905636103e666916c4c
Stéphane Juban <stephane.juban@gmail.com>
2016-01-02 15:32:15 +0000

    Add some explanations on homepage

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,6 +2,10 @@ class PageController < ApplicationController
 
   # This is our homepage
   def index
+    @url_all    = "#{request.base_url}/api/v1/posts"
+    @url_author = "#{request.base_url}/api/v1/posts?author=Anonyme"
+    @url_date   = "#{request.base_url}/api/v1/posts?from=#{1.week.ago.to_date}"
+    @url_full   = "#{request.base_url}/api/v1/posts?author=Anonyme&from=#{2.weeks.ago.to_date}&to=#{1.day.ago.to_date}"
   end
 
   # Get the 200 last posts from VDM
