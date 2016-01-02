d70984b47342ba82ca35797ba53a3dc401d2236d
Stéphane Juban <stephane.juban@gmail.com>
2016-01-02 10:16:37 +0000

    Display every posts and store them in database

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -11,16 +11,37 @@ class PageController < ApplicationController
     require 'open-uri'
 
     # The current page to fetch
-    page    = 0
-    # The array of VDM
-    @vdm = Array.new
+    page = 0
 
-    while @vdm.length < NB_OF_VDM_TO_GET do
-      @page = Nokogiri::HTML(open("http://www.viedemerde.fr/?page=#{page}"))
-      @page.css('.post.article').each do |article|
-        @vdm.length < NB_OF_VDM_TO_GET ? @vdm << article : break
+    # First of all empty the database
+    Post.delete_all
+    # The number of records
+    count = 0
+
+    while count < NB_OF_VDM_TO_GET do
+      html = Nokogiri::HTML(open("http://www.viedemerde.fr/?page=#{page}"))
+
+      html.css('.post.article').each do |article|
+        # Get the content of the VDM
+        content = article.css('p').first.text
+
+        # Get the infos splitted (e.g. "Le jj/mm/yyyy à hh:mm - category - par author")
+        infos   = article.css('.date .right_part p')[1].text.split(' - ')
+        date    = infos.first
+        author  = infos.last.match(/par (\S+)/i)[1]
+
+        if count < NB_OF_VDM_TO_GET
+          Post.create(:content => content, :date => date, :author => author)
+          count += 1
+        else
+          break
+        end
+
       end
+
+      page += 1
     end
+
   end
 
 end
